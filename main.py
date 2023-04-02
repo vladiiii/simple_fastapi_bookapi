e8806529b018a6601a7141027334930cc85e7063
Vladi <vladiiiiiiiii@gmail.com>
2023-04-02 20:18:36 +0300

    added book-index

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+BOOKDB = [
+    "The Great Gatsby",
+    "Atomic Habits",
+    "Norwegian Wood",
+]
+
 
 @app.get("/")
-async def root():
-    return {"messsage": "Hello", "var": 1234}
+async def home():
+    return {"messsage": "Welcome to the best BookStore"}
+
+
+@app.get("/list-books")
+async def list_books():
+    return {"books": BOOKDB}
+
+
+@app.get("/book-index/{index}")
+async def book_index(index: int):
+    if index < 0 or index > len(BOOKDB):
+        raise HTTPException(404, f"Index {index} out of range {len(BOOKDB)}.")
+    else:
+        return {"book": BOOKDB[index]}
