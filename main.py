632a763214c20c3b2a08a666db99dcf38f83b80b
Vladi <vladiiiiiiiii@gmail.com>
2023-04-02 20:38:30 +0300

    added post method

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI, HTTPException
+import json
+import os
+import random
 
 app = FastAPI()
 
+BOOKS_FILE = "books.json"
 BOOKDB = [
     "The Great Gatsby",
     "Atomic Habits",
     "Norwegian Wood",
 ]
+
+if os.path.exists(BOOKS_FILE):
+    with open(BOOKS_FILE, "r") as f:
+        BOOKDB = json.load(f)
 
 
 @app.get("/")
@@ -25,3 +33,16 @@ async def book_index(index: int):
         raise HTTPException(404, f"Index {index} out of range {len(BOOKDB)}.")
     else:
         return {"book": BOOKDB[index]}
+
+
+@app.get("/get-random-book")
+async def get_random_book():
+    return {"book": random.choice(BOOKDB)}
+
+
+@app.post("/add-book")
+async def add_book(book: str):
+    BOOKDB.append(book)
+    with open(BOOKS_FILE, "w") as f:
+        json.dump(BOOKDB, f)
+    return {"message": f"Added '{book}'"}
