6a4b958cb55f8853198a9a66cc0095f4b4a44677
Vladi <vladiiiiiiiii@gmail.com>
2023-04-03 15:33:40 +0300

    added get-book method

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from typing import Optional, Literal
+from uuid import uuid4
 import json
 import os
 import random
 
 app = FastAPI()
 
+
+class Book(BaseModel):
+    name: str
+    price: float
+    genre: Literal["fiction", "non-fiction"]
+    book_id: Optional[str] = uuid4().hex
+
+
 BOOKS_FILE = "books.json"
-BOOKDB = [
-    "The Great Gatsby",
-    "Atomic Habits",
-    "Norwegian Wood",
-]
+BOOKDB = []
 
 if os.path.exists(BOOKS_FILE):
     with open(BOOKS_FILE, "r") as f:
@@ -41,8 +49,19 @@ async def get_random_book():
 
 
 @app.post("/add-book")
-async def add_book(book: str):
-    BOOKDB.append(book)
+async def add_book(book: Book):
+    book.book_id = uuid4().hex
+    json_book = jsonable_encoder(book)
+    BOOKDB.append(json_book)
     with open(BOOKS_FILE, "w") as f:
         json.dump(BOOKDB, f)
-    return {"message": f"Added '{book}'"}
+    return {"message": f"Added '{book}'", "id": book.book_id}
+
+
+@app.get("/get-book")
+async def get_book(book_id: str):
+    for book in BOOKDB:
+        if book.book_id == book_id:
+            return book
+        else:
+            raise HTTPException(404, f"Book does not exsist {book_id}")
